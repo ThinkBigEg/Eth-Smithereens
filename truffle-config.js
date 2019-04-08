0ba4dec5f5db3db210d5b170085d9d9c784413c0
Omar-Khaled2198 <omar21621@gmail.com>
2019-04-08 18:15:03 +0200

    add networks config

--- truffle-config.js
+++ truffle-config.js
@@ -3,5 +3,13 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks:{
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     }
+    },
   contracts_build_directory: path.join(__dirname, "client/src/contracts")
+  
 };
