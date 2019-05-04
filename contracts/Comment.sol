118a167c5f81bc448084db7a7fc801287acd1ed4
Omar-Khaled2198 <omar21621@gmail.com>
2019-05-04 22:35:40 +0200

    add comments

--- contracts/Comment.sol
+++ contracts/Comment.sol
@@ -1,35 +1,30 @@
 pragma solidity ^0.5.0;
 import "./User.sol";
-import "./Comment.sol";
-contract Post {
 
+contract Comment {
+    
     User owner;
     string public text;
     uint256 public timestamp;
-    address[] comments;
-
+    
     uint public totalVotes = 0;
     uint public totalVotesValue = 0;
     mapping(address => uint) public votes;
 
-    constructor (address user_contract_address,string memory post_text) public {
-        text = post_text;
+    constructor(address user_contract_address,string memory comment_text) public {
+        text = comment_text;
         timestamp = now;
         owner = User(user_contract_address);
     }
-
-    function getPost() public view returns(address,string memory,string memory,uint256){
+    
+    
+    function getComment() public view returns(address,string memory,string memory,uint256 ){
         return (
         address(owner),
         owner.name(),
         text,
         timestamp
             );
-    }
-
-    function createComment(address user_contract_address,string memory comment_text)public{
-        Comment comment = new Comment(user_contract_address,comment_text);
-        comments.push(address(comment));
     }
 
     function vote(address userContractAddress,uint voteValue) public {
@@ -40,5 +35,5 @@ contract Post {
         totalVotes++;
 
     }
-
+    
 }
